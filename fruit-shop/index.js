02fc204027c99686289d48e733d138441ca75594
apoorvnema <apoorvnema38@gmail.com>
2024-03-26 19:00:44 +0530

    Task 11: - filter functionality

--- fruit-shop/index.js
+++ fruit-shop/index.js
@@ -1,26 +1,32 @@
-const li = document.getElementsByClassName('fruit');
-for (let i = 0; i < li.length; i++) {
-    const edit = document.createElement("button");
-    const editText = document.createTextNode("Edit");
-    edit.append(editText);
-    edit.className = "edit-btn";
-    li[i].appendChild(edit);
-}
-// Implement the code as in video but with one extra 'Edit' button in 'li'
+const desc = document.createElement("input");
 const form = document.querySelector("form");
+const btn = document.querySelector("button");
+desc.id = "description";
+desc.type = "text";
+form.insertBefore(desc, btn);
+// Show the fruit description in italics on the next line
 const fruits = document.querySelector(".fruits");
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    const fruitToAdd = document.getElementById("fruit-to-add");
-    const newFruit = document.createElement("li");
-    newFruit.className = "fruit";
-    newFruit.innerHTML = fruitToAdd.value + '<button class="delete-btn">x</button><button class="edit-btn">Edit</button>';
-    fruits.appendChild(newFruit);
+    const li = document.createElement("li");
+    const fruitToAdd = document.querySelector("#fruit-to-add");
+    li.className = "fruit";
+    li.innerHTML = fruitToAdd.value + `<p style='font-style:italic;'>${desc.value}</p><button class='delete-btn'>x</button>`;
+    fruits.appendChild(li);
 });
-
-fruits.addEventListener("click", function (event) {
-    if (event.target.classList.contains('delete-btn')) {
-        const fruitToDelete = event.target.parentElement;
-        fruits.removeChild(fruitToDelete);
+// Create a filter that shows only those fruits whose either name or description or both matches the entered text
+const filter = document.querySelector("#filter");
+filter.addEventListener('keyup', function (event) {
+    const fruit = document.querySelectorAll(".fruit");
+    const filterText = event.target.value.toLowerCase();
+    for (let i = 0; i < fruit.length; i++) {
+        const fruitText = fruit[i].firstChild.textContent.toLowerCase();
+        const fruitDescription = fruit[i].querySelector("p") ? fruit[i].querySelector("p").textContent.toLowerCase() : "";
+        if (fruitText.indexOf(filterText) == -1 && fruitDescription.indexOf(filterText) == -1) {
+            fruit[i].style.display = "none";
+        }
+        else {
+            fruit[i].style.display = "flex";
+        }
     }
 });
