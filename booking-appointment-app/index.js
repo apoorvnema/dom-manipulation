7827a03fd8d8f7759d39793b100aeadbccb6368b
apoorvnema <apoorvnema38@gmail.com>
2024-03-26 19:21:34 +0530

    Task 15: - adding delete functionality

--- booking-appointment-app/index.js
+++ booking-appointment-app/index.js
@@ -1,16 +1,23 @@
 function handleFormSubmit(event) {
     event.preventDefault();
     let myObj = {
+        username: event.target.username.value,
         email: event.target.email.value,
         phone: event.target.phone.value,
-        username: event.target.username.value,
     }
     let myObj_serialised = JSON.stringify(myObj);
+    localStorage.setItem(event.target.email.value, myObj_serialised);
     const ul = document.querySelector("ul");
     const li = document.createElement("li");
-    li.textContent = event.target.username.value + "-" + event.target.email.value + "-" + event.target.phone.value;
+    li.innerHTML = event.target.username.value + "-" + event.target.email.value + "-" + event.target.phone.value + "<button type='button' class='delete-button' data-email='" + event.target.email.value + "'>Delete</button>";
+    li.id = event.target.email.value;
     ul.appendChild(li);
-    localStorage.setItem(event.target.email.value, myObj_serialised);
+
+    const deleteButton = li.querySelector(".delete-button");
+    deleteButton.addEventListener("click", function () {
+        document.getElementById(event.target.email.value).remove();
+        localStorage.removeItem(event.target.email.value);
+    });
 }
 
 module.exports = handleFormSubmit;
