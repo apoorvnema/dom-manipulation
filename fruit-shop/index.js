49aeb54a708b0b824dd5cd7a3d57ea483af03c11
apoorvnema <apoorvnema38@gmail.com>
2024-03-26 18:45:45 +0530

    Task 6: - using querySelector & querySelectorAll

--- fruit-shop/index.js
+++ fruit-shop/index.js
@@ -1,4 +1,32 @@
-const fruit = document.getElementsByTagName("li");
-fruit[4].style.color = "blue";
-for (let i = 0; i < fruit.length; i++)
-    fruit[i].style.fontStyle = "italic";
+const mainHeading = document.querySelector("#main-heading");
+mainHeading.style.textAlign = "center";
+const fruits = document.querySelector(".fruits");
+fruits.style.backgroundColor = "gray";
+fruits.style.padding = "30px";
+fruits.style.margin = "30px";
+fruits.style.width = "50%";
+fruits.style.borderRadius = "5px";
+fruits.style.liststyleType = "none";
+const basketHeading = document.querySelector("h2");
+basketHeading.style.marginteft = "30px";
+const fruitItems = document.querySelectorAll(".fruit");
+
+for (let i = 0; i < fruitItems.length; i++) {
+    fruitItems[i].style.backgroundColor = "white";
+    fruitItems[i].style.padding = "10px";
+    fruitItems[i].style.margin = "10px";
+    fruitItems[i].style.borderRadius = "5px";
+}
+
+const OddFruitItems = document.querySelectorAll(".fruit:nth-child(odd)");
+
+for (let i = 0; i < OddFruitItems.length; i++) {
+    OddFruitItems[i].style.backgroundColor = "lightgray";
+}
+// Write answer to the questions asked below:
+basketHeading.style.color = "brown";
+const EvenFruitItems = document.querySelectorAll(".fruit:nth-child(even)");
+for (let i = 0; i < EvenFruitItems.length; i++) {
+    EvenFruitItems[i].style.backgroundColor = "brown";
+    EvenFruitItems[i].style.color = "white";
+}
