bef7b81da452003e3988e1ed2ff0e5c913cddb8e
apoorvnema <apoorvnema38@gmail.com>
2024-03-26 19:22:49 +0530

    Task 16: - adding edit functionality

--- booking-appointment-app/index.js
+++ booking-appointment-app/index.js
@@ -9,14 +9,26 @@ function handleFormSubmit(event) {
     localStorage.setItem(event.target.email.value, myObj_serialised);
     const ul = document.querySelector("ul");
     const li = document.createElement("li");
-    li.innerHTML = event.target.username.value + "-" + event.target.email.value + "-" + event.target.phone.value + "<button type='button' class='delete-button' data-email='" + event.target.email.value + "'>Delete</button>";
+    li.innerHTML = `${event.target.username.value} - ${event.target.email.value} - ${event.target.phone.value} <button type='button' class='delete-button' data-email='${event.target.email.value}'>Delete</button> <button type='button' class='edit-button' data-email='${event.target.email.value}'>Edit</button>`;
     li.id = event.target.email.value;
     ul.appendChild(li);
 
     const deleteButton = li.querySelector(".delete-button");
     deleteButton.addEventListener("click", function () {
-        document.getElementById(event.target.email.value).remove();
-        localStorage.removeItem(event.target.email.value);
+        const email = deleteButton.getAttribute('data-email');
+        document.getElementById(email).remove();
+        localStorage.removeItem(email);
+    });
+
+    const editButton = li.querySelector(".edit-button");
+    editButton.addEventListener("click", function () {
+        const email = editButton.getAttribute('data-email');
+        const getItem = JSON.parse(localStorage.getItem(email));
+        document.getElementById("username").value = getItem.username;
+        document.getElementById("email").value = getItem.email;
+        document.getElementById("phone").value = getItem.phone;
+        document.getElementById(email).remove();
+        localStorage.removeItem(email);
     });
 }
 
